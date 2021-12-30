f103ce657d2877d27a0a4ecd0d8e1c472fff603f
Mayank <mayanksharma301199@gmail.com>
2021-12-30 13:55:07 +0530

    Adding env

--- Django/TodoDjango/TodoDjango/settings.py
+++ Django/TodoDjango/TodoDjango/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os 
 import datetime
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-emb0es5&rcxi1q-0hd--xxp=^($x)ewvgm36rp7&k0c=el8fh*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
